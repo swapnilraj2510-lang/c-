cb43e4bcc7271766c95d572ce08673672b6c4cf6
swapnilraj2510-lang <swapnilraj2510@gmail.com>
2026-08-17 18:10:36 +0000

    operator

---
+++ operator.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    cout << "Addition = " << a + b << endl;
+    cout << "Subtraction = " << a - b << endl;
+    cout << "Multiplication = " << a * b << endl;
+    cout << "Division = " << a / b << endl;
+    cout << "Remainder = " << a % b << endl;
+
+    return 0;
+}
