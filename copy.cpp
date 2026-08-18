e05ffc73a863a22e29ee7a93de441ae63f65c28d
swapnilraj2510-lang <swapnilraj2510@gmail.com>
2026-08-18 16:29:27 +0000

    copyconstructor

---
+++ copy.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    int x;
+
+    
+    A() {
+        x = 0;
+    }
+
+    
+    A(const A &obj) {
+        x = obj.x;
+    }
+};
+
+int main() {
+    
+    A a1;
+    a1.x = 10;
+
+    cout << "a1's x = " << a1.x << endl;
+
+    
+    A a2(a1);
+
+    cout << "a2's x = " << a2.x << endl;
+
+    return 0;
+}
